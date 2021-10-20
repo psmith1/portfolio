377fb141a6912b803088a8c004beb846b9e1f1ae
Peter Smith <petergbsmith@gmail.com>
2021-10-20 14:32:53 -0500

    back button

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import { Icon } from '@iconify/react';
 import bxMenu from '@iconify-icons/bx/bx-menu';
 import { useState, useEffect } from 'react';
 import ReactGA from 'react-ga4';
-import { useHistory } from 'react-router-dom'
 
 
 function usePageViews() {
@@ -49,30 +48,6 @@ const AppRouter = () => {
 }
 
 function App() {
-const [ locationKeys, setLocationKeys ] = useState([])
-const history = useHistory()
-
-useEffect(() => {
-  return history.listen(location => {
-    if (history.action === 'PUSH') {
-      setLocationKeys([ location.key ])
-    }
-
-    if (history.action === 'POP') {
-      if (locationKeys[1] === location.key) {
-        setLocationKeys(([ _, ...keys ]) => keys)
-
-        // Handle forward event
-
-      } else {
-        setLocationKeys((keys) => [ location.key, ...keys ])
-
-        // Handle back event
-
-      }
-    }
-  })
-}, [ locationKeys, ])
 
 const [openMenu, setOpenMenu] = useState(false);
 
